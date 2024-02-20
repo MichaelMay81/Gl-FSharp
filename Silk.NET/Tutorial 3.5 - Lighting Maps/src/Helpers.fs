2071c1accc1cddf9337eb67bc47145bec5d60670
Michael May <mike@ull.at>
2024-02-20 23:07:49 +0100

    3.5 using 1.4 reference

--- Silk.NET/Tutorial 3.5 - Lighting Maps/src/Helpers.fs
+++ Silk.NET/Tutorial 3.5 - Lighting Maps/src/Helpers.fs
@@ -5,15 +5,3 @@ open System
 
 let degreesToRadians (degrees:float32) =
     MathF.PI / 180f * degrees
-
-let resultToOption = function
-    | Error error ->
-        printfn "Error: %s" error
-        None
-    | Ok value ->
-        Some value
-
-let printError (result: Result<'T, string>) : unit =
-    result
-    |> Result.mapError (printfn "Error: %s")
-    |> ignore
