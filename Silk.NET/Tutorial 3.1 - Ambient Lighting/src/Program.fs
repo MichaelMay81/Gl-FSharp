242626e49d11a917e15d7b3bc9e03cd8dd5e9325
Michael May <mike@ull.at>
2024-02-23 21:46:07 +0100

    3.1 using 1.4 and 2.2 references and include original comments

--- Silk.NET/Tutorial 3.1 - Ambient Lighting/src/Program.fs
+++ Silk.NET/Tutorial 3.1 - Ambient Lighting/src/Program.fs
@@ -1,11 +1,12 @@
 ﻿open System.Numerics
-open System.Linq
 open Silk.NET.Maths
 open Silk.NET.Windowing
 open Silk.NET.OpenGL
-open Tutorial1_4_Abstractions
 open Silk.NET.Input
 open System
+
+open Tutorial1_4_Abstractions
+open Tutorial2_2_Camera
 
 type Model = {
     Window: IWindow
@@ -18,55 +19,54 @@ type Model = {
 
     vbo: BufferObject<float32>
     vao: VertexArrayObject
-    LightingShaderOpt: Shader option
-    LampShaderOpt: Shader option
+    LightingShader: Shader
+    LampShader: Shader
 
     Camera: Camera
-    Zoom: float32
     LastMousePosition: Vector2 option }
 
 // The quad vertices data.
 let private vertices = [|
-    -0.5f; -0.5f; -0.5f
-    0.5f; -0.5f; -0.5f
-    0.5f;  0.5f; -0.5f
-    0.5f;  0.5f; -0.5f
-    -0.5f;  0.5f; -0.5f
-    -0.5f; -0.5f; -0.5f
+    -0.5f; -0.5f; -0.5f;
+     0.5f; -0.5f; -0.5f;
+     0.5f;  0.5f; -0.5f;
+     0.5f;  0.5f; -0.5f;
+    -0.5f;  0.5f; -0.5f;
+    -0.5f; -0.5f; -0.5f;
 
-    -0.5f; -0.5f;  0.5f
-    0.5f; -0.5f;  0.5f
-    0.5f;  0.5f;  0.5f
-    0.5f;  0.5f;  0.5f
-    -0.5f;  0.5f;  0.5f
-    -0.5f; -0.5f;  0.5f
+    -0.5f; -0.5f;  0.5f;
+     0.5f; -0.5f;  0.5f;
+     0.5f;  0.5f;  0.5f;
+     0.5f;  0.5f;  0.5f;
+    -0.5f;  0.5f;  0.5f;
+    -0.5f; -0.5f;  0.5f;
 
-    -0.5f;  0.5f;  0.5f
-    -0.5f;  0.5f; -0.5f
-    -0.5f; -0.5f; -0.5f
-    -0.5f; -0.5f; -0.5f
-    -0.5f; -0.5f;  0.5f
-    -0.5f;  0.5f;  0.5f
+    -0.5f;  0.5f;  0.5f;
+    -0.5f;  0.5f; -0.5f;
+    -0.5f; -0.5f; -0.5f;
+    -0.5f; -0.5f; -0.5f;
+    -0.5f; -0.5f;  0.5f;
+    -0.5f;  0.5f;  0.5f;
 
-    0.5f;  0.5f;  0.5f
-    0.5f;  0.5f; -0.5f
-    0.5f; -0.5f; -0.5f
-    0.5f; -0.5f; -0.5f
-    0.5f; -0.5f;  0.5f
-    0.5f;  0.5f;  0.5f
+     0.5f;  0.5f;  0.5f;
+     0.5f;  0.5f; -0.5f;
+     0.5f; -0.5f; -0.5f;
+     0.5f; -0.5f; -0.5f;
+     0.5f; -0.5f;  0.5f;
+     0.5f;  0.5f;  0.5f;
 
-    -0.5f; -0.5f; -0.5f
-    0.5f; -0.5f; -0.5f
-    0.5f; -0.5f;  0.5f
-    0.5f; -0.5f;  0.5f
-    -0.5f; -0.5f;  0.5f
-    -0.5f; -0.5f; -0.5f
+    -0.5f; -0.5f; -0.5f;
+     0.5f; -0.5f; -0.5f;
+     0.5f; -0.5f;  0.5f;
+     0.5f; -0.5f;  0.5f;
+    -0.5f; -0.5f;  0.5f;
+    -0.5f; -0.5f; -0.5f;
 
-    -0.5f;  0.5f; -0.5f
-    0.5f;  0.5f; -0.5f
-    0.5f;  0.5f;  0.5f
-    0.5f;  0.5f;  0.5f
-    -0.5f;  0.5f;  0.5f
+    -0.5f;  0.5f; -0.5f;
+     0.5f;  0.5f; -0.5f;
+     0.5f;  0.5f;  0.5f;
+     0.5f;  0.5f;  0.5f;
+    -0.5f;  0.5f;  0.5f;
     -0.5f;  0.5f; -0.5f |]
 
 let keyDown (window:IWindow) (_:IKeyboard) (key:Key) (_:int) =
@@ -78,11 +78,11 @@ let keyDown (window:IWindow) (_:IKeyboard) (key:Key) (_:int) =
 let onClose (model:Model) =
     model.vbo |> BufferObjects.dispose
     model.vao |> VertexArrayObjects.dispose
-    model.LightingShaderOpt |> Option.iter Shaders.dispose
-    model.LampShaderOpt |> Option.iter Shaders.dispose
+    model.LightingShader |> Shaders.dispose
+    model.LampShader |> Shaders.dispose
 
 let OnMouseWheel (model:Model) (scrollWheel:ScrollWheel) : Model =
-    { model with Zoom = Math.Clamp (model.Zoom - scrollWheel.Y, 1f, 45f) }
+    { model with Camera = model.Camera |> Cameras.modifyZoom scrollWheel.Y }
 
 let onMouseMove (model:Model) (position:Vector2) : Model =
     let lookSensitivity = 0.1f
@@ -108,47 +108,37 @@ let onRender (model:Model) (deltaTime:float) =
         (ClearBufferMask.ColorBufferBit |||
         ClearBufferMask.DepthBufferBit)
 
-    let viewMat = model.Camera |> Cameras.viewMatrix
-    let projMat =
-        Matrix4x4.CreatePerspectiveFieldOfView (
-            model.Zoom |> degreesToRadians,
-            (float32 model.Width) / (float32 model.Height),
-            0.1f,
-            100f )
+    let lightingShaderWerror func = model.LightingShader |> func |> printError
+    let lampShaderWerror func = model.LampShader |> func |> printError
 
-    let setUniform name value shader =
-        shader |> Option.iter (Shaders.setUniform name value >> printError)
-        shader
+    let viewMat = model.Camera |> Cameras.viewMatrix
+    let projMat = model.Camera |> Cameras.projectionMatrix model.Width model.Height
 
     //Binding and using our VAO and shader.
     model.vao |> VertexArrayObjects.bind
-    model.LightingShaderOpt |> Option.iter Shaders.useProgram
+    model.LightingShader |> Shaders.useProgram
 
     //Setting a uniform.
-    model.LightingShaderOpt
-    |> setUniform "uModel" (25f |> degreesToRadians |> Matrix4x4.CreateRotationY |> Shaders.M4)
-    |> setUniform "uView" (viewMat |> Shaders.M4)
-    |> setUniform "uProjection" (projMat |> Shaders.M4)
-    |> setUniform "objectColor" (Vector3 (1f, 0.5f, 031f) |> Shaders.V3)
-    |> setUniform "lightColor" (Vector3.One |> Shaders.V3)
-    |> ignore
+    lightingShaderWerror <| Shaders.setUniformMat4 "uModel" (25f |> degreesToRadians |> Matrix4x4.CreateRotationY)
+    lightingShaderWerror <| Shaders.setUniformMat4 "uView" viewMat
+    lightingShaderWerror <| Shaders.setUniformMat4 "uProjection" projMat
+    lightingShaderWerror <| Shaders.setUniformVec3 "objectColor" (Vector3 (1f, 0.5f, 031f))
+    lightingShaderWerror <| Shaders.setUniformVec3 "lightColor" Vector3.One
     
     model.Gl.DrawArrays (
         PrimitiveType.Triangles,
         0,
         vertices |> Array.length |> uint)
 
-    model.LampShaderOpt |> Option.iter Shaders.useProgram
+    model.LampShader |> Shaders.useProgram
     let lampMatrix =
         Matrix4x4.Identity
         * Matrix4x4.CreateScale 0.2f
         * Matrix4x4.CreateTranslation (Vector3 (1.2f, 1f, 2f))
 
-    model.LampShaderOpt
-    |> setUniform "uModel" (lampMatrix |> Shaders.M4)
-    |> setUniform "uView" (viewMat |> Shaders.M4)
-    |> setUniform "uProjection" (projMat |> Shaders.M4)
-    |> ignore
+    lampShaderWerror <| Shaders.setUniformMat4 "uModel" lampMatrix
+    lampShaderWerror <| Shaders.setUniformMat4 "uView" viewMat
+    lampShaderWerror <| Shaders.setUniformMat4 "uProjection" projMat
     
     model.Gl.DrawArrays (
         PrimitiveType.Triangles,
@@ -180,10 +170,10 @@ let onUpdate (model:Model) (deltaTime:float) : Model =
     { model with Camera = {model.Camera with Position = cameraPosition} }
 
 
-let onLoad (window:IWindow) : Model =
+let onLoad (window:IWindow) : Model option =
     let inputContext = window.CreateInput ()
-    let keyboard = inputContext.Keyboards.FirstOrDefault ()
-    let mouse = inputContext.Mice.FirstOrDefault ()
+    let keyboard = inputContext.Keyboards |> Seq.head
+    let mouse = inputContext.Mice |> Seq.head
     //mouse.Cursor.CursorMode <- CursorMode.Raw
     
     let gl = GL.GetApi window
@@ -206,21 +196,29 @@ let onLoad (window:IWindow) : Model =
         { Position= Vector3.UnitZ * 6f
           Up= Vector3.UnitY
           Yaw= -90f
-          Pitch= 0f }
+          Pitch= 0f
+          Zoom = 45f }
 
-    {   Window = window
-        Gl = gl
-        vbo = vbo
-        vao = vao
-        LightingShaderOpt = lightingShaderOpt
-        LampShaderOpt = lampShaderOpt
-        Camera = camera
-        Zoom = 45f
-        Width = window.Size.X
-        Height = window.Size.Y
-        Keyboard = keyboard
-        Mouse = mouse
-        LastMousePosition = None }
+    match lightingShaderOpt, lampShaderOpt with
+    | Some lightingShader, Some lampShader ->
+        {   Window = window
+            Gl = gl
+            vbo = vbo
+            vao = vao
+            LightingShader = lightingShader
+            LampShader = lampShader
+            Camera = camera
+            Width = window.Size.X
+            Height = window.Size.Y
+            Keyboard = keyboard
+            Mouse = mouse
+            LastMousePosition = None } |> Some
+    | _ ->
+        vbo |> BufferObjects.dispose
+        vao |> VertexArrayObjects.dispose
+        lightingShaderOpt |> Option.iter Shaders.dispose
+        lampShaderOpt |> Option.iter Shaders.dispose
+        None
 
 let onFramebufferResize (gl:GL) (size:Vector2D<int>) =
     gl.Viewport (0, 0, uint size.X, uint size.Y)
@@ -234,16 +232,20 @@ let main _ =
     use window = Window.Create options
 
     window.add_Load (fun _ ->
-        let mutable model = onLoad window
-        
-        model.Keyboard.add_KeyDown (keyDown window)
-        model.Mouse.add_MouseMove (fun _ pos -> model <- onMouseMove model pos)
-        model.Mouse.add_Scroll (fun _ scrollWheel -> model <- OnMouseWheel model scrollWheel)
+        match onLoad window with
+        | Some model ->
+            let mutable model = model
+            
+            model.Keyboard.add_KeyDown (keyDown window)
+            model.Mouse.add_MouseMove (fun _ pos -> model <- onMouseMove model pos)
+            model.Mouse.add_Scroll (fun _ scrollWheel -> model <- OnMouseWheel model scrollWheel)
 
-        window.add_Update (fun deltaTime -> model <- onUpdate model deltaTime)
-        window.add_Render (onRender model)
-        window.add_Closing (fun _ -> onClose model)
-        window.add_FramebufferResize (onFramebufferResize model.Gl))
+            window.add_Update (fun deltaTime -> model <- onUpdate model deltaTime)
+            window.add_Render (onRender model)
+            window.add_Closing (fun _ -> onClose model)
+            window.add_FramebufferResize (onFramebufferResize model.Gl)
+        | None ->
+            window.Close () )
     
     window.Run ()
     window.Dispose ()
