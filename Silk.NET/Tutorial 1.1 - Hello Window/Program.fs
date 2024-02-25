fc5d4f39f91a677b307ed35b9e09e415b0cdaec3
Michael May <mike@ull.at>
2024-02-25 19:51:37 +0100

    1.1 include original comments

--- Silk.NET/Tutorial 1.1 - Hello Window/Program.fs
+++ Silk.NET/Tutorial 1.1 - Hello Window/Program.fs
@@ -1,6 +1,4 @@
-﻿module Tutorial1_1_Hello_Window
-
-open Silk.NET.Input
+﻿open Silk.NET.Input
 open Silk.NET.Maths
 open Silk.NET.Windowing
 
@@ -11,8 +9,7 @@ let keyDown (window:IWindow) (_:IKeyboard) (key:Key) (_:int) =
     | _ -> ()
 
 let onLoad (window:IWindow) =
-    printfn "Load!"
-
+    //Set-up input context.
     let input = window.CreateInput ()
     input.Keyboards
     |> Seq.iter (fun keyboard ->
@@ -20,13 +17,21 @@ let onLoad (window:IWindow) =
     
 [<EntryPoint>]
 let main _ =
+    //Create a window.
     use window:IWindow = Window.Create WindowOptions.Default
     window.Size <- Vector2D<int> (800, 600)
-    window.Title <- "My first Silk.NET program!"
+    window.Title <- "LearnOpenGL with Silk.NET"
 
+    //Assign events.
     window.add_Load (fun _ -> onLoad window)
-    window.add_Update (fun _ -> printfn "Update!")
-    window.add_Render (fun _ -> printfn "Render!")
+    window.add_Update (fun _ -> printfn "Here all updates to the program should be done.")
+    window.add_Render (fun _ -> printfn "Here all rendering should be done.")
 
+    //Run the window.
     window.Run ()
+
+    // window.Run() is a BLOCKING method - this means that it will halt execution of any code in the current
+    // method until the window has finished running. Therefore, this dispose method will not be called until you
+    // close the window.
+    window.Dispose ()
     0
